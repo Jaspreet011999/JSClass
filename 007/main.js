5462dabd1b00908e675e62cd9db6a05aa93cb11d
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-02-08 09:47:54 -0800

    fixing files

--- 007/main.js
+++ 007/main.js
@@ -1,8 +1,8 @@
 let studying  = "for the exam";
 console.log("studying is for the exam which is typeof ",(typeof studying))
 
-let numberofstudents = 20;
-console.log("numberofstudents is 20 which is typeof " ,(typeof numberofstudents))
+let numberOfStudents = 20;
+console.log("numberofstudents is 20 which is typeof " ,(typeof numberOfStudents))
 
 let multiplechoicequestion = false;
  console.log("multiplechoicequestion is false which is typeof " ,(typeof multiplechoicequestion))
