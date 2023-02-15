2c1bb4d39a4294ff4b4ccf8e99c438ccfc913f18
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-02-14 17:09:55 -0800

    fun with functions

---
+++ functions/main.js
@@ -0,0 +1,9 @@
+// functions is great
+function doMath (a,b){
+    console.log(`this is the result ${a+b}`)
+    console.log(`this is the result ${a-b}`)
+    console.log(`this is the result ${a*b}`)
+    console.log(`this is the result ${a/b}`)
+
+}
+doMath(3,5)
