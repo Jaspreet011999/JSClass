33d52f0c63d3db6b6ac0813fe303dc266a54ece0
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-02-15 11:09:40 -0800

    my functions

---
+++ week_6/main.js
@@ -0,0 +1,10 @@
+function salesTotal(){
+    let price = 7.89;
+    let quantity = 4;
+    productPrice = price * quantity;
+    salesTotalValue = productPrice. toFixed(2);
+    return salesTotal;
+
+}
+salesTotal();
+console.log(salesTotalValue);
