5b5dd0df298e1e446e84b69ad0948dc81c7f889a
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-02-23 10:31:49 -0800

    writing the mileage function

--- 012-more-functions/main.js
+++ 012-more-functions/main.js
@@ -7,5 +7,20 @@ const updateDOM = (input) => {
   divEl.appendChild(j)  
 
 }
-const  calculateSpeed = ( distance, time ,speed = 65) => distance/time * speed
-updateDOM(calculateSpeed(50 ,20 ,65))
+//const  calculateSpeed = ( distance, time ,speed = 65) => distance/time * speed
+//updateDOM(calculateSpeed(50 ,20 ,65))
+
+const calculateSpeed = ( distance,time) => {
+  const DTS = distance/time
+  return DTS
+}
+
+
+
+
+const myDTS = calculateSpeed(300,20)
+
+updateDOM("My speed is " +myDTS + " km/h")
+
+
+
