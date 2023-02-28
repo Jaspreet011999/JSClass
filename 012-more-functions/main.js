3b6bf25b1978a416c878ffb948f1f6bbd63b82c5
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-02-27 16:59:26 -0800

    writing the average MPG using for loop

--- 012-more-functions/main.js
+++ 012-more-functions/main.js
@@ -14,13 +14,18 @@ const updateDOM = (input) => {
 const trackMPGCost = (miles ,gallons, price ) => {
   const MPG = miles/gallons
   const tripCost = MPG * price
-myArr .push(MPG,tripCost)
+   myArr.push(MPG,tripCost)
+
+  updateDOM(`Your MPG is ${myArr[0]} miles per gallon and your trip cost is $${myArr[1]}`);
+ 
 
 }
 
 trackMPGCost(200,20,4.59)
+trackMPGCost(300,10,3.59)
+updateDOM(myArr)
 
-updateDOM(`MPG is ${myArr[0]} miles per gallon and your trip cost is $${myArr[1]}`);
+
 
  
 
--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -1,40 +1,46 @@
 //update the DOM
 
-const myArr= []
+const myMPG = []
+const myTripCost = []
 
-const updateDOM = (input) => {
+ const updateDOM = (input) => {
   const divEl = document.querySelector('#output')
   const j = document.createElement('div')
   j.textContent=input
   divEl.appendChild(j)  
 
 }
-//const  calculateSpeed = ( distance, time ,speed = 65) => distance/time * speed
-//updateDOM(calculateSpeed(50 ,20 ,65))
 
-const calculateSpeed = ( distance,time,speed) => {
-  const DTS = distance/time
-  const carMiles = DTS * speed
-  updateDOM (`DTS is ${DTS} and car Miles is ${carMiles}`)
-  myArr.push( DTS,carMiles)
+
+const trackMPGCost = (miles ,gallons, price ) => {
+  const MPG = miles/gallons
+  const tripCost = MPG * price
+  myMPG.push(MPG)
+   myTripCost.push(tripCost)
+
+  updateDOM(`Your MPG is ${myMPG} miles per gallon and your trip cost is $${myTripCost}`);
+ 
+
 }
-
 const calculateAvg = () => {
-    const avgDTS = (myArr[0]+myArr[2])/2
-    updateDOM( avgDTS)
+  let totalMPG = 0
+    // const avgMPG = (myMPG[0] + myMPG[2])/2
+    // updateDOM(`Average Miles per gallon is ${avgMPG}`)
+    for(let i = 0;i < myMPG.length ; i++){
+    totalMPG = totalMPG + myMPG[i]
+    }
+    let avgMPG = totalMPG/myMPG.length 
+    updateDOM (`Average MPG is ${avgMPG}`)
+
 }
 
+
+
+trackMPGCost(200,20,4.59)
+trackMPGCost(300,10,3.59)
+trackMPGCost(500,30,2.59)
+trackMPGCost(800,70,1.59)
+trackMPGCost(600,40,6.59)
+trackMPGCost(800,90,9.59)
 calculateAvg()
-
-
-
-
-calculateSpeed(400,15,5.50)
- calculateSpeed (420,11,6)
- updateDOM(myArr)
-
-
-
-
-
 
