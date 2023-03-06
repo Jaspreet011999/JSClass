a44202942c884a093f856d603db3abb001c158a9
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-03-06 10:09:20 -0800

    my loops

---
+++ 014-more-looping/main.js
@@ -0,0 +1,66 @@
+//update the DOM
+
+const MY_MPG = []
+const MY_TRIP_COST = []
+
+ const updateDOM = (input) => {
+  const divEl = document.querySelector('#output')
+  const j = document.createElement('div')
+  j.textContent=input
+  divEl.appendChild(j)  
+
+}
+
+
+const trackMPGandCost = (miles ,gallons, price ) => {
+  const MPG = miles/gallons
+  const tripCost = MPG * price
+  MY_MPG.push(MPG)
+   MY_TRIP_COST.push(tripCost)
+
+   
+
+  updateDOM(`Your MPG is ${MY_MPG} miles per gallon and your trip cost is $${MY_TRIP_COST}`);
+ 
+
+}
+
+const calculateSum = (arr) => {
+    let sum = 0
+    //for (let i=0; i < arr.length;i++) {
+     // sum = sum + arr[i]
+  
+    //}
+    //arr.array.forEach(element => {
+     // sum += element
+      
+   // });
+    for (value of arr) {
+      sum += value
+    }
+    return sum
+  }
+
+
+  const calculateAvgValue = () => {
+    let valueCost = 0
+  for( let i = 0 ; i < MY_TRIP_COST. length; i++)
+  valueCost += MY_TRIP_COST [i]
+  let avgValue = valueCost/MY_TRIP_COST.length
+  updateDOM(`Average ValueCost is ${avgValue}`)
+  
+  
+  }
+  
+  
+  
+  
+  
+  
+  trackMPGandCost(200,20,4.59)
+  trackMPGandCost(300,10,3.59)
+  trackMPGandCost(500,30,2.59)
+  trackMPGandCost(800,70,1.59)
+  trackMPGandCost(600,40,6.59)
+  trackMPGandCost(800,90,9.59)
+  calculateAvgValue()
---
+++ 014-my-loops/main.js
@@ -0,0 +1,15 @@
+let r = 1, k = 20;
+// while loop counting number of students r = 1 to 20
+while (r <= k) {
+    console.log (r);
+    r += 1;
+}
+
+let result = '';
+let a = 1;
+
+do {
+    a = a + 1;
+    result = result + a;
+}while (a < 2);
+  console.log (result);
