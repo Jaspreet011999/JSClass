dda4b26780161406e41f77037695063ccec55312
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-03-07 13:29:43 -0800

    fixing issues

--- 014-my-loops/main.js
+++ 014-my-loops/main.js
@@ -1,15 +1,16 @@
-let r = 1, k = 20;
+let r = 1,
+  k = 20;
 // while loop counting number of students r = 1 to 20
 while (r <= k) {
-    console.log (r);
-    r += 1;
+  console.log(r);
+  r += 1;
 }
 
-let result = '';
+let result = "";
 let a = 1;
 
 do {
-    a = a + 1;
-    result = result + a;
-}while (a < 2);
-  console.log (result);
+  a = a + 1;
+  result = result + a;
+} while (a < 2);
+console.log(result);
--- 015-playing-with-forms/main.js
+++ 015-playing-with-forms/main.js
@@ -1,76 +1,60 @@
-const FormEL = document.getElementById('form-input')
-
+const FormEL = document.getElementById("form-input");
 
 //update the DOM
 
-const MY_MPG = []
-const MY_TRIP_COST = []
+const MY_MPG = [];
+const MY_TRIP_COST = [];
 
- const updateDOM = (input) => {
-  const divEl = document.querySelector('#output')
-  const j = document.createElement('div')
-  j.textContent=input
-  divEl.appendChild(j)  
+const updateDOM = (input) => {
+  const divEl = document.querySelector("#output");
+  const j = document.createElement("div");
+  j.textContent = input;
+  divEl.appendChild(j);
+};
 
-}
+const calculateSpeed = (distance, time) => {
+  const speed = distance / time;
 
-
-const calculateSpeed = (distance ,time) => {
-  const speed =distance/time
-  
-
-   
-
-  updateDOM(`${distance} i will take ${time} seconds was speed ${speed}`)
- 
-
-}
+  updateDOM(`${distance} i will take ${time} seconds was speed ${speed}`);
+};
 
 //const calculateSum = (arr) => {
-   // let sum = 0
-    //for (let i=0; i < arr.length;i++) {
-     // sum = sum + arr[i]
-  
-    //}
-    //arr.array.forEach(element => {
-     // sum += element
-      
-   // });
-    //for (value of arr) {
-     // sum += value
-    //}
-   // return sum
- // }
+// let sum = 0
+//for (let i=0; i < arr.length;i++) {
+// sum = sum + arr[i]
 
- 
+//}
+//arr.array.forEach(element => {
+// sum += element
 
-  //const calculateAvgValue = () => {
-   // let valueCost = 0
- // for( let i = 0 ; i < MY_TRIP_COST. length; i++)
- // valueCost += MY_TRIP_COST [i]
-  //let avgValue = valueCost/MY_TRIP_COST.length
-  //updateDOM(`Average ValueCost is ${avgValue}`)
-  
-  
- // }
-  
-  
-  
-  FormEL.addEventListener('submit',(e) =>{
-    e.preventDefault()
-    const distance= parseInt (e.target.distance.value)
-    const time= parseInt (e.target.time.value)
-    
+// });
+//for (value of arr) {
+// sum += value
+//}
+// return sum
+// }
 
-   calculateSpeed(distance,time)
+//const calculateAvgValue = () => {
+// let valueCost = 0
+// for( let i = 0 ; i < MY_TRIP_COST. length; i++)
+// valueCost += MY_TRIP_COST [i]
+//let avgValue = valueCost/MY_TRIP_COST.length
+//updateDOM(`Average ValueCost is ${avgValue}`)
 
-  })
-  
-  
-  //tracMPGandCost(200,20,4.59)
-  //trackMPGandCost(300,10,3.59)
-  //trackMPGandCost(500,30,2.59)
-  //trackMPGandCost(800,70,1.59)
-  //trackMPGandCost(600,40,6.59)
-  //trackMPGandCost(800,90,9.59)
-  //calculateAvgValue()
+// }
+
+FormEL.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const distance = parseInt(e.target.distance.value);
+  const time = parseInt(e.target.time.value);
+
+  calculateSpeed(distance, time);
+});
+
+//tracMPGandCost(200,20,4.59)
+//trackMPGandCost(300,10,3.59)
+//trackMPGandCost(500,30,2.59)
+//trackMPGandCost(800,70,1.59)
+//trackMPGandCost(600,40,6.59)
+//trackMPGandCost(800,90,9.59)
+//calculateAvgValue()
