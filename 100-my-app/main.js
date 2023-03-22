60db9c3732c601566c9c2e6ea60b0f253af103ef
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-03-21 23:00:18 -0700

    my decision logic

---
+++ 100-my-app/main.js
@@ -0,0 +1,29 @@
+// Declare the variables
+const money = 100;
+const missingGroceries = ['vegetables', 'fruits', 'snacks'];
+// const affordableGrocery = ['Walmart', 'Costco','Dollar tree'];
+const discountStore = true; 
+const isFresh = true;
+const expiryDate = 1; //number of days before it expires
+
+
+if(money >= 100 && missingGroceries.length !== 0){
+     console.log("I am going to the grocery store");
+
+     if(discountStore){
+         console.log("We are going to this grocery store because groceries will be affordable")
+
+         if(isFresh && expiryDate >= 5){
+         console.log("Let's proceed and buy groceries")
+         }
+         else{
+            console.log("I will not buy from this store because food is not fresh or is expiring soon")
+         }
+     }
+     else{
+        console.log("I will find a store with discount")
+     }
+ }
+ else{
+  console.log("I am not going to the grocery store");
+ }
---
+++ 100-my-app/readme.md
@@ -0,0 +1,4 @@
+# Week 10 Decision Making CRUD App
+- When buying groceries, I normally check whether I have enough money. I then check what groceries I am missing or the groceries I need. The next thing I will check for is an affordable grocery store. When I get to the store, I will check the quality of the groceries, and this includes things like: Are they fresh? When is the expiry date? After I have confirmed these, I will buy the grocerries.  
+
+- I will use nested if statements for my logic
