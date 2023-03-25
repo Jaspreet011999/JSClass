e2f001ed5b738edc968e2d69ffd749a24ac573e2
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-03-24 23:34:05 -0700

    my functions

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,34 +1,55 @@
-// Declare the variables
+// Declare the global variables
+
 const money = 100;
-const missingGroceries = ['vegetables', 'fruits', 'snacks'];
-// const affordableGrocery = ['Walmart', 'Costco','Dollar tree'];
-const discountStore = true; 
-const isFresh = true;
-const expiryDate = 1; //number of days before it expires
-const total = 0;
-const discount = 0.1;
-const itemPrice = 10
+const missingGroceries = ["vegetables", "fruits", "snacks"];
 
+function buyGroceries(
+  itemPrice,
+  quantity,
+  discount,
+  discountStore,
+  isFresh,
+  expiryDate
+) {
+  let decision;
 
-if(money >= 100 && missingGroceries.length !== 0){
-     console.log("I am going to the grocery store");
+  if (money >= 100 && missingGroceries.length !== 0) {
+    decision = "I am going to the grocery store";
 
-     if(discountStore){
-         console.log("We are going to this grocery store because groceries will be affordable")
+    if (discountStore) {
+      decision =
+        "We are going to this grocery store because groceries will be affordable";
 
-         if(isFresh && expiryDate >= 5){
-         total = (itemPrice * quantity) * (1-discount)
-         console.log("Let's proceed and buy groceries")
+      if (isFresh) {
+        if (expiryDate >= 5) {
+          total = itemPrice * quantity * ((100 - discount) / 100);
+          decision = "Let's proceed and buy groceries";
+        } else {
+          decision =
+            "I will not buy from this store groceries are expriring soon";
+        }
+      } else {
+        decision = "I will not buy from this store because food is not fresh";
+      }
+    } else {
+      decision = "I will find a store with discount";
+    }
+  } else {
+    decision = "I am not going to the grocery store";
+  }
+  const groceriesData = {
+    itemPrice: itemPrice,
+    discountStore: discountStore,
+    discount: discount,
+    isFresh: isFresh,
+    expiryDate: expiryDate,
+    total: total,
+    decision: decision,
+  };
 
-         }
-         else{
-            console.log("I will not buy from this store because food is not fresh or is expiring soon")
-         }
-     }
-     else{
-        console.log("I will find a store with discount")
-     }
- }
- else{
-  console.log("I am not going to the grocery store");
- }
+  return groceriesData;
+}
+
+buyGroceries(90, 10, 15, true, true, 6);
+buyGroceries(90, 10, 15, true, false, 6);
+buyGroceries(90, 10, 15, true, true, 1);
