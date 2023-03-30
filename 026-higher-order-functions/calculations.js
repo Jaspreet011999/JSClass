2f9f5d71a156f1acc73ff22c50c1e169c90291c9
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-03-29 21:43:55 -0700

    intro to higher order functions

---
+++ 026-higher-order-functions/calculations.js
@@ -0,0 +1,35 @@
+function updateDOM(input) {
+  const divEl = document.querySelector("#output");
+  const j = document.createElement("div");
+  j.textContent = input;
+  divEl.appendChild(j);
+}
+
+
+function trackMPGandCost(miles, gallons, price) {
+  const MPG = Number((miles / gallons).toFixed(2));
+  const tripCost = Number((gallons * price).toFixed(2));
+  //updateDOM(`Miles per gallon is ${MPG} and trip cost ${tripCost}`, `#output`);
+  return {
+    miles: miles,
+    gallons: gallons,
+    price: price,
+    MPG: MPG,
+    tripCost: tripCost,
+  };
+}
+
+function calculateAverages(MY_TRIP_COST) {
+  let totalMPG = 0;
+  let totalTripCost = 0;
+  MY_TRIP_COST.forEach((obj) => {
+    totalMPG += obj.MPG;
+    totalTripCost += obj.tripCost;
+  });
+  const averageMPG = Number((totalMPG / MY_TRIP_COST.length).toFixed(2));
+  const averageTripCost = Number((totalTripCost / MY_TRIP_COST.length).toFixed(2));
+  updateDOM(`Average MPG is ${averageMPG}`);
+  updateDOM(`Average Trip Cost is ${averageTripCost}`);
+}
+
+export { updateDOM, trackMPGandCost, calculateAverages };
---
+++ 026-higher-order-functions/readme.md
@@ -0,0 +1,4 @@
+# WEEK 12 HIGHER ORDER FUNCTIONS
+# THE INTRO VIDEO
+- higher order function takes another function as a parameter(or returns a function)
+-examples of higher order functions include setTimeout(), array.filter()
