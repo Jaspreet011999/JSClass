3019e953d594bbb9e28010f15534a2899fd0263d
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-03-29 22:05:05 -0700

    implement reduce for sum

--- 026-higher-order-functions/calculations.js
+++ 026-higher-order-functions/calculations.js
@@ -20,12 +20,18 @@ function trackMPGandCost(miles, gallons, price) {
 }
 
 function calculateAverages(MY_TRIP_COST) {
-  let totalMPG = 0;
-  let totalTripCost = 0;
-  MY_TRIP_COST.forEach((obj) => {
-    totalMPG += obj.MPG;
-    totalTripCost += obj.tripCost;
-  });
+  // let totalMPG = 0;
+  // let totalTripCost = 0;
+  // MY_TRIP_COST.forEach((obj) => {
+  //   totalMPG += obj.MPG;
+  //   totalTripCost += obj.tripCost;
+  // });
+  const totalMPG = MY_TRIP_COST.reduce(function(sum, obj){
+    return {
+      MPG: sum.MPG + obj.MPG,
+      tripCost: sum.tripCost + obj.tripCost
+    }
+  },0)
   const averageMPG = Number((totalMPG / MY_TRIP_COST.length).toFixed(2));
   const averageTripCost = Number((totalTripCost / MY_TRIP_COST.length).toFixed(2));
   updateDOM(`Average MPG is ${averageMPG}`);
