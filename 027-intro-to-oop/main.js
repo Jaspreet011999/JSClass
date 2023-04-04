7ec80b303b878676f92fd6279e61b5646dfe68d4
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-04-04 09:55:22 -0700

    my attempt at oop coding challenge

--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,25 +1,57 @@
 class Person {
   constructor(firstName, lastName, address) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.address = address;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._address = address;
+  }
+  getFirstName() {
+    return `${this._firstName}`;
+  }
+  setFirstName(newFirstName) {
+    this._firstName = newFirstName;
+  }
+  getLasttName() {
+    return `${this._lastName}`;
+  }
+  setLastName(newLastName) {
+    this._lastName = newLastName;
+  }
+  getAddress() {
+    return `${this._address}`;
+  }
+  setAddress(newAddress) {
+    this._address = newAddress;
   }
   showStatus() {
-    console.log(`${this.firstName} lives at ${this.address}`);
+    console.log(`${this.getFirstName()} lives at ${this.getAddress()}`);
   }
 }
 
 class Student extends Person {
-    constructor(firstName,lastName,address,classList){
-      super(firstName,lastName,address)
-      this.classList = classList
-    }
-    showStatus(){
-        console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`);
-    }
+  constructor(firstName, lastName, address, classList) {
+    super(firstName, lastName, address);
+    this.classList = classList;
+  }
+  getClassList() {
+    return `${this.classList}`;
+  }
+  setClassList(newClassList) {
+    this.classList = newClassList;
+  }
+  showStatus() {
+    console.log(
+      `${this.getFirstName()} lives at ${this.getAddress()} is taking ${this.getClassList()}`
+    );
+  }
 }
 
-const jaspreet = new Student("Jaspreet", "Kaur", "Private, can't tell you",["English", "CTA200", "CTA300"]);
+const jaspreet = new Student("Jaspreet", "Kaur", "Private, can't tell you", [
+  "English",
+  "CTA200",
+  "CTA300",
+]);
 jaspreet.showStatus();
 const jane = new Person("Jane", "Wood", "127 Street");
 jane.showStatus();
+
+console.log(jaspreet.getFirstName());
