1a7af6d8a1de6be2d48e9929610504ea5930eb0c
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-04-04 08:00:41 -0700

    student subclass

--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -9,7 +9,17 @@ class Person {
   }
 }
 
-const jaspreet = new Person("Jaspreet", "Kaur", "Private, can't tell you");
+class Student extends Person {
+    constructor(firstName,lastName,address,classList){
+      super(firstName,lastName,address)
+      this.classList = classList
+    }
+    showStatus(){
+        console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`);
+    }
+}
+
+const jaspreet = new Student("Jaspreet", "Kaur", "Private, can't tell you",["English", "CTA200", "CTA300"]);
 jaspreet.showStatus();
 const jane = new Person("Jane", "Wood", "127 Street");
 jane.showStatus();
--- 027-intro-to-oop/readme.md
+++ 027-intro-to-oop/readme.md
@@ -1,2 +1,4 @@
 # Week 13
 - We are now creating objects using a constructor method, which takes arguments, inside which we declare properties of our object.  Previously we used to create objects by writing the key, and value. 
+
+- Yes I understand the prototype chain. The prototype of student is person, and that of person is object. 
