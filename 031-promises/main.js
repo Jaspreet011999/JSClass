b3ce9af1dc8eb81f1489087acdd08e9fe8b37dc1
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-04-19 11:30:57 -0700

    intro to promises

---
+++ 031-promises/main.js
@@ -0,0 +1,35 @@
+function getWeather() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("SLIGHT RAIN");
+    }, 100);
+  });
+}
+
+function getWeatherIcon(weather) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      switch (weather) {
+        case "Sunny":
+          resolve("🏖️");
+          break;
+        case "Cloudy":
+          resolve("⛅");
+          break;
+        case "Rainy":
+          resolve("☔");
+          break;
+        default:
+          reject("NO ICON FOUND");
+      }
+    }, 100);
+  });
+}
+
+function onSuccess(data) {
+  console.log("Success", data);
+}
+function onError(error) {
+  console.log("Error", error);
+}
+getWeather().then(getWeatherIcon).then(onSuccess, onError);
