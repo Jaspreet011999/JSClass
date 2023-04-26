d7d6eab752494f83ba96fb3a8ac30e1328a70c7a
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-04-26 09:33:32 -0700

    actual example

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,14 +1,23 @@
-function getData() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(46)
-        }, 1)
-    })
-}
+// function getData() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(46)
+//         }, 1)
+//     })
+// }
 
 async function start(){
-const result = await getData()
-console.log(result);
+const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+const result = await data.json()
+console.log(result.properties.periods[1].shortForecast);
+}
+
+async function start2(){
+    fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+    .then(data => data.json())
+    .then(result => {
+        console.log(result.properties.periods[1].shortForecast);
+    })
 }
 
 start()
