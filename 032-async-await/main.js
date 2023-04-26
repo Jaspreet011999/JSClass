d2eb87d14040aacaebded32fa4c1d742d92f8e96
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-04-26 09:56:45 -0700

    try/catch

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,23 +1,48 @@
-// function getData() {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve(46)
-//         }, 1)
-//     })
-// }
-
-async function start(){
-const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
-const result = await data.json()
-console.log(result.properties.periods[1].shortForecast);
-}
-
-async function start2(){
-    fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
-    .then(data => data.json())
-    .then(result => {
-        console.log(result.properties.periods[1].shortForecast);
+function getData() {
+    return new Promise(function (resolve,reject){
+        setTimeout(() => {
+            reject('Something went wrong')
+        }, 1)
     })
 }
 
+function onSuccess(){
+    console.log(`Success: $(result)`);
+}
+function onError(){
+    console.log(`ERROR: $(error)`);
+}
+
+async function start(){
+    try{
+        const result = await getData()
+       onSuccess()
+    } catch(error){
+        onError()
+    }
+}
+// async function start2(){
+//     try{
+//         const result = await getData()
+//     } catch(error){
+//         console.log(`ERROR: $(error)`);
+//     }
+// }
 start()
+// start2()
+
+// async function start(){
+// const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+// const result = await data.json()
+// console.log(result.properties.periods[1].shortForecast);
+// }
+
+// async function start2(){
+//     fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+//     .then(data => data.json())
+//     .then(result => {
+//         console.log(result.properties.periods[1].shortForecast);
+//     })
+// }
+
+// start()
