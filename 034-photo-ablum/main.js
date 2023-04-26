cc2df73708e2f4ec94f2217f83b7349939ab30e4
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-04-26 10:22:43 -0700

    photo album my code

---
+++ 034-photo-ablum/main.js
@@ -0,0 +1,33 @@
+
+
+  // Fetch images from the API
+  async function fetchImages() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+      const images = await response.json();
+      return images;
+    } catch (error) {
+      console.error('Failed to load images:', error);
+      return [];
+    }
+  }
+  // Render images on the DOM
+  async function renderImages() {
+    const imageContainer = document.querySelector('.image');
+    const images = await fetchImages();
+
+    // Clear existing images
+    imageContainer.innerHTML = '';
+
+    // Render fetched images
+    images.forEach(image => {
+      const imgElement = document.createElement('img');
+      imgElement.src = image.url;
+      imgElement.alt = image.title;
+      imageContainer.appendChild(imgElement);
+    });
+  }
+
+  // Call the renderImages function to fetch and render images
+  renderImages();
+
