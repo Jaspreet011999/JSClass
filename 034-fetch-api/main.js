f1a46589f6daaaed57515ed089cbd89dbeec7c8f
Jaspreet011999 <109932441+Jaspreet011999@users.noreply.github.com>
2023-05-03 11:10:00 -0700

    fetch header object

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,10 +1,24 @@
-const url = " https://www.thenewsapi.com/v1/news/top?api_token="
+const url = " https://www.thenewsapi.com/v1/news/top?api_token=9605"
 // "http://worldtimeapi.org/api/timezone/America/Los_Angeles"
 
+const  request = new Request(url, {
+    headers: {
+        'Authorization' : 'Bearer '
+    }
+   }) 
 async function getData(){
-   const response = await fetch(url)
-   const data = await response.json();
-   console.log(data); 
+   try {
+    const response = await fetch(request)
+    const data = await response.json();
+    if(response.status === 200){
+        console.log('Success', data); 
+    } else{
+        console.log('Server Error', data.error.message);
+    }
+    
+   }catch (error){
+    console.log('Error', error);
+   }
 }
 
 getData()
--- 034-fetch-api/readme.md
+++ 034-fetch-api/readme.md
@@ -1,2 +1,5 @@
 # Week 16
 - Yes I usually run my code in the local server and see if it is running or if there are any errors in the console. If there are any errors I usually use the console the line at which the error occurs. 
+
+# Notes
+-
